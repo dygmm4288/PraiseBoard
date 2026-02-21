6397422826df934d6c8899ff23f65917c8251358
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2026-02-21 17:36:11 +0900

    chore: 파일 구조 개선

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,8 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
-import { HapticTab } from "@/components/haptic-tab";
-import { IconSymbol } from "@/components/ui/icon-symbol";
+import { HapticTab } from "@/shared/components/haptic-tab";
+import { IconSymbol } from "@/shared/components/ui/icon-symbol";
 
 export default function TabLayout() {
   return (
