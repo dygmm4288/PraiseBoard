26ea245f5bd107547a0b3e81ec04f51924272b8b
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2026-03-10 22:40:41 +0900

    feat(intro): refresh intro copy content

--- features/intro/components/intro-content.tsx
+++ features/intro/components/intro-content.tsx
@@ -7,31 +7,27 @@ type Props = {
 };
 
 export default function IntroContent({ currentIndex }: Props) {
-  const texts = [
-    [
-      "내 하루는 누가 알아주나요?",
-      "결과보다 과정을 먼저 봐주는 친구를 만나보세요.",
-    ],
-    [
-      "아무도 모르는 사소한 일도 괜찮아요.",
-      "작은 성취를 기록하고 스스로를 칭찬해 주세요.",
-    ],
+  const title = [
+    "내 하루는\n 누가 알아주나요?",
+    "나만 아는 사소한 일도 괜찮아요",
+  ][currentIndex];
+  const content = [
+    "과도한 결과 중독 사회에서 우리는 지치기 쉬워요.결과보다 과정의 의미를 찾고 따뜻하게 지켜봐주는 고래친구 두잉을 만나보세요.",
+    "당신이 원하는 무엇이든 기록할 수 있어요. 행동을 하나씩 쌓아가며 스스로 칭찬하고 보상을 얻어봐요. 두잉이 곁에서 지켜줄거예요.",
   ][currentIndex];
 
   return (
-    <View className='flex flex-col gap-5 '>
-      <AppText tone='muted'>{currentIndex + 1} / 2 </AppText>
-      <View>
-        {texts.map((v, i) => (
-          <AppText
-            key={`intro_content${currentIndex}${i}`}
-            variant='title'
-            weight='bold'
-            className='whitespace-normal'>
-            {v}
-          </AppText>
-        ))}
-      </View>
+    <View className="flex flex-col gap-[20px]">
+      <AppText
+        variant="title"
+        weight="bold"
+        className="whitespace-normal text-center"
+      >
+        {title}
+      </AppText>
+      <AppText variant="body2" className="text-center">
+        {content}
+      </AppText>
     </View>
   );
 }
