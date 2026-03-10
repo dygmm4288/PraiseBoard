c4f6520b54c80a3442f22f4d9c1fc035e6fa117f
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2026-03-10 22:31:08 +0900

    refactor(theme): extract color tokens into shared constant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { COLOR } = require("./shared/constants/colors.constant.js");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,26 +11,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gray: {
-          900: "#111111",
-          800: "#272728",
-          700: "#323234",
-          500: "#8E8E93",
-          300: "#D1D1D6",
-          200: "#EEEEEE",
-          100: "#F6F6F6",
-        },
-        white: "#FFFFFF",
-        primary: {
-          800: "#2C2643",
-          700: "#44307A",
-          600: "#5839A8",
-          500: "#7145D6",
-          400: "#8C5CFF",
-          300: "#B696FF",
-          200: "#D2BFFF",
-          100: "#E8DEFF",
-        },
+        ...COLOR,
         secondary: {
           // TODO 추가 예정
         },
