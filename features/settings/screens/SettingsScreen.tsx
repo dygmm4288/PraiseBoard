bae6e9b14d50e80ed794ee67563c1a690d17b7a5
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2026-04-05 19:12:53 +0900

    feat: setting header link

--- features/settings/screens/SettingsScreen.tsx
+++ features/settings/screens/SettingsScreen.tsx
@@ -2,7 +2,8 @@ import { Icon } from "@/assets/icons";
 import { useUser } from "@/services/user";
 import { Screen } from "@/shared/ui";
 import ScreenHeader from "@/shared/ui/screen-header";
-import { View } from "react-native";
+import { useNavigation } from "expo-router";
+import { Pressable, View } from "react-native";
 import SettingAuth from "../components/sections/setting-auth";
 import SettingEnv from "../components/sections/setting-env";
 import SettingInfo from "../components/sections/setting-info";
@@ -11,9 +12,17 @@ import SettingProfile from "../components/sections/setting-profile";
 
 const SettingsScreen = () => {
   const { authState } = useUser();
+  const navigation = useNavigation();
   return (
     <Screen>
-      <ScreenHeader left={<Icon name="ChevronLeft" />} title="설정" />
+      <ScreenHeader
+        left={
+          <Pressable onPress={() => navigation.goBack()}>
+            <Icon name="ChevronLeft" />
+          </Pressable>
+        }
+        title="설정"
+      />
       <View className="flex flex-col gap-[24px] mt-[10px]">
         <SettingProfile />
         <SettingNotification />
