9e574a66de7ee116e63046a2cc2cddd16e844bda
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2026-05-16 12:55:31 +0900

    fix(icons): allow fnb icons to inherit color

--- assets/icons/icon.tsx
+++ assets/icons/icon.tsx
@@ -1,16 +1,17 @@
 import { ICONS, IconName } from "./registry";
+import type { SvgProps } from "react-native-svg";
 
-type Props = {
+type Props = SvgProps & {
   name: IconName;
   size?: number;
   width?: number;
   height?: number;
 };
 
-const Icon = ({ name, size = 24, width, height }: Props) => {
+const Icon = ({ name, size = 24, width, height, ...props }: Props) => {
   const Svg = ICONS[name];
 
-  return <Svg width={width || size} height={height || size} />;
+  return <Svg width={width || size} height={height || size} {...props} />;
 };
 
 export default Icon;
