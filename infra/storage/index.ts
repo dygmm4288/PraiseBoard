9709e510550308b469451707cf465c2fde897cbb
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2026-07-14 23:01:38 +0900

    fix(storage): use web storage on web

--- infra/storage/index.ts
+++ infra/storage/index.ts
@@ -1,7 +1,7 @@
+import { Platform } from "react-native";
 import { asyncStorageImpl } from "./storage.impl.async-storage";
 import { webStorageImpl } from "./storage.impl.web-storage";
 import { IStorage } from "./storage.interface";
-import { isStorybookEnabled } from "@/shared/constants/environment";
 
 export const localStorage: IStorage =
-  isStorybookEnabled ? webStorageImpl : asyncStorageImpl;
+  Platform.OS === "web" ? webStorageImpl : asyncStorageImpl;
